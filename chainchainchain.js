762370c2f55cef8f7c022aa2ba78f765a56223f8
jrvieira <zero@zero.local>
2016-11-21 03:23:28 +0000

    Protected chain and made array accessible
    - chain is settable as second argument
    - new get function returns chain
    - protected constructor properties

--- chainchainchain.js
+++ chainchainchain.js
@@ -22,53 +22,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/*TODO
-	chain
-	dechain
-	inchain
-	rechain
-	get
-	getget
-	raw
-	rawraw
-	findloop-allowloops livro[89]
-
-	method to retrieve the object from wich the requested prop came from (first prop in raw / rawraw)
-
-OK	module export
-
-	call vs apply vs bindy
-
-	make warning when multiple instances of same object in chain
-	make ch 'private' livro[89]
-
-	objects only mode
-
-	evaluation usage: o.chain.val ?
-*/
-
 'use strict';
 
 module.exports = chainchainchain;
 //defaults
 chainchainchain.settings = {
 	ownchainonly: true,
-	allowloops: false,
-	
+	allowloops: false
 }
 
-var chi = Symbol();
+//o[chi] = Chainchainchain {} //Chainchainchain[chi] = []
+var chi = Symbol('chi');
 
 var handle = {
 	get: function (target, prop) {
+		if (prop === chi) return target.ch;
 		//get youngest
-		if( prop in target.o ) return target.o[prop];
+		if (prop in target.o) return target.o[prop];
 		console.log('// ch get young', prop);
 		//return target[prop];
 	},
 	set: function (target, prop, value) {
+		if (prop === chi) return target.ch = value;
 		//set youngest
-		if( prop in target.o ) return target.o[prop] = value;
+		if (prop in target.o) return target.o[prop] = value;
 		console.log('// ch set young', prop);
 		//return target[prop] = value;
 	}
@@ -76,87 +53,37 @@ var handle = {
 //this makes the handler replaceable
 var handler = handle;
 
-function Ch (o) {
-	this.o = o;
-	this.ch = [];
+function Chainchainchain (o, ch) {
+	Object.defineProperties(this, {
+		o: {
+			value: o
+		},
+		ch: {
+			value: ch || [],
+			writable: true
+		}
+	});
 }
 
-function chainchainchain (o) {
-	//if already initd return o's Ch
-	if (o[chi] instanceof Ch) return o[chi];
+function chainchainchain (o, ch) {
+	//validate typeof ch
+	if (ch && !(ch instanceof Array)) {
+		throw new TypeError(ch+' is not an Array');
+	}
+	//if already initd return o's Chainchainchain
+	if (o[chi] instanceof Chainchainchain) {
+		o[chi][chi] = ch || o[chi][chi];
+		return o[chi];
+	}
 
 	console.log('// ch init');
 
-	var proxy = new Proxy(new Ch(o), handler);
-	//typeof chi === 'symbol'
+	var proxy = new Proxy(new Chainchainchain(o, ch), handler);
 	o[chi] = proxy;
 
 	return proxy;
-
 }
 
-const ch_ = Object.freeze({
-	//adds objects to chain or returns chain when called with no arguments
-	add: function () {
-		if(!arguments.length){ //returns chain when called with no arguments
-			return this._ch;
-		} else {
-			this._ch = this._ch || []; //guarantees chain array
-			for (let i = 0; i < arguments.length; i ++) { //adds objects to chain
-				this._ch.push(arguments[i]);
-			}
-			//FINDLOOPS
-			return this;
-		}
-	},
-	//removes objects from chain
-	remove: function () {
-		if(this._ch){
-			if(!arguments.length){ //clears the chain when called with no arguments
-				this._ch = [];
-			}else{
-				for (let i = 0; i < arguments.length; i ++) {	
-					this._ch = this._ch.filter(function (o) {
-					    return o !== arguments[i];
-					});
-				}
-			}
-		}
-		return this;
-	},
-	//prepends objects to chain
-	prepend: function () {
-		this._ch = this._ch || []; //guarantees chain array
-		for (let i = arguments.length; i > 0; i --) { //prepends objects to chain
-			this._ch.unshift(arguments[i-1]);
-		}
-		//FINDLOOPS
-		return this;
-	},
-	//replaces objects in chain
-	replace: function (x,o) {
-		this._ch = this._ch || []; //guarantees chain array
-		while(!(this._ch.indexOf(x) < 0)){ //replaces objects in chain
-			this._ch.splice(this._ch.indexOf(x),1,o);
-		}
-		//test this for multiple instances of same object in chain
-		//FINDLOOPS
-		return this;
-	}/*,
-	getproxy: function (caller) {
-
-		return proxy = new Proxy({}, {
-			get: function (obj, prop) {
-				console.log(caller, obj, prop);
-				return 'gotten value';
-			}
-			//,
-			//set: function (obj, prop, value) {
-			//	console.log(caller, t, n, v);
-			//	//set value
-			//}
-		})
-
-	}
-	*/
-});
+chainchainchain.get = function (o) {
+	return o[chi][chi];
+}
